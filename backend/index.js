d806e6e241f1720d23bf167c55c99415f0bbe782
kenneth luzolo <kenneth.nepsther@gmail.com>
2025-04-20 23:25:36 +0100

    :sparkles: feat: check auth

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
 
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json()); // Middleware to parse JSON request bodies: This is used to parse incoming JSON requests and make the data available in req.body.
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
+app.use(cookieParser()); // Middleware to parse cookies in the request: This is used to parse cookies attached to the client request object.
 
 
 app.use("/api/auth", authRoutes);
